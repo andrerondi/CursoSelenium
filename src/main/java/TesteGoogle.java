7828817a8a2f19acf17acaee4727c551583ab4bf
Andre de Castro Rondi <andre_castro_rondi@hotmail.com>
2021-12-08 17:14:37 -0400

    Configurando JUnit

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,12 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteGoogle {
 	
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
-		System.out.println(driver.getTitle());
+//		System.out.println(driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
 	}
 
 }
